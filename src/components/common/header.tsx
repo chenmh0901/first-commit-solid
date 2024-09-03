906b5b6d120ec7640e08afaaee8c8dbc16aaf666
陈明浩 <1210204146@zust.edu.cn>
2024-09-03 14:21:06 +0800

    update header md

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -5,13 +5,13 @@ export const Header = () => {
   return (
     <>
       <div class="sticky left-0 top-0 h-16 w-screen border-b backdrop-blur-lg">
-        <div class='max-w-[1200px] mx-auto flex justify-between items-center h-full'>
-          <a class="w-1/6 flex items-center gap-2 hover:cursor-pointer">
+        <div class='max-w-[1200px] mx-auto px-4 flex justify-between items-center h-full'>
+          <a class="flex items-center gap-2 hover:cursor-pointer">
             <img src="/public/favicon.ico" alt="logo" class="w-7" />
-            <span class="text-lg font-bold">First commit</span>
+            <span class="text-lg font-bold hidden md:block">First commit</span>
           </a>
           <Nav />
-          <div class='flex gap-2'>
+          <div class="gap-2 hidden md:flex">
             <Button variant={'default'}>Sign in</Button>
             <Button variant={'default'}>Sign up</Button>
           </div>
