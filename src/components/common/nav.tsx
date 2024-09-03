df86f62f206c19fbe19fa60dd5e1a0a5fe9f82d2
陈明浩 <1210204146@zust.edu.cn>
2024-09-03 16:01:57 +0800

    update md:nav

--- src/components/common/nav.tsx
+++ src/components/common/nav.tsx
@@ -1,3 +1,7 @@
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "~/components/ui/dropdown-menu"
+import { Button } from '~/components/ui/button'
+import { Icon } from '@iconify-icon/solid'
+
 export interface NaviItem {
   name?: string;
   path: string;
@@ -24,12 +28,30 @@ export const Nav = () => {
   ];
 
   return (
-    <ul class="w-1/3 h-full flex flex-1 items-center justify-center">
-      {naviItems.map((item: NaviItem) => (
-        <li class="mx-4">
-          <a href={item.path} class="hover:text-gray">{item.name || item.path}</a>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul class="w-1/3 h-full flex-1 items-center justify-center hidden md:flex">
+        {naviItems.map((item: NaviItem) => (
+          <li class="mx-4">
+            <a href={item.path} class="hover:text-gray">{item.name || item.path}</a>
+          </li>
+        ))}
+      </ul>
+      <div class="md:hidden ml-auto">
+        <DropdownMenu placement="bottom">
+          <DropdownMenuTrigger>
+            <Button variant={'ghost'} size={'icon'}>
+              <Icon icon="ic:sharp-menu" height={'20px'} width={'20px'} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            {naviItems.map((item: NaviItem) => (
+              <DropdownMenuItem>
+                <a href={item.path} class="hover:text-gray">{item.name || item.path}</a>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </>
   );
 };
